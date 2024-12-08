9453e404006602063a8cbb134ed19de51d58b45f
Fadil Nohur <fadil.nohur@outlook.com>
2024-12-08 14:41:28 +0000

    Advent of Code Year 2024 Day 5
    - Fix tests to correctly run for part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -126,9 +126,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_one(&advent_of_code::template::read_file_part(
+        let result = part_two(&advent_of_code::template::read_file_part(
             "examples", DAY, 1,
         ));
-        assert_eq!(result, Some(143));
+        assert_eq!(result, Some(123));
     }
 }
