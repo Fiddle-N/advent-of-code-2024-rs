a084e3a3a25fd1dd0b64e4a610d416a0d5968d46
Fadil Nohur <fadil.nohur@outlook.com>
2024-12-12 22:36:49 +0000

    Advent of Code Year 2024 Day 12
    - Code reformatting

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,7 +1,9 @@
-use std::{collections::{HashMap, HashSet}, ops::Add};
+use std::{
+    collections::{HashMap, HashSet},
+    ops::Add,
+};
 
 advent_of_code::solution!(12);
-
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 struct Point {
@@ -29,7 +31,7 @@ const DIRECTIONS: [[Point; 2]; 4] = [
 
 struct GardenRegion {
     region: HashSet<Point>,
-    corners: u32
+    corners: u32,
 }
 
 struct Garden {
@@ -46,84 +48,119 @@ fn parse(input: &str) -> HashMap<Point, char> {
     let mut map = HashMap::new();
     for (y, row) in input.lines().enumerate() {
         for (x, c) in row.chars().enumerate() {
-            map.insert(Point{x: x.try_into().expect("Fit in u32"), y: y.try_into().expect("Fit in u32")}, c);
+            map.insert(
+                Point {
+                    x: x.try_into().expect("Fit in u32"),
+                    y: y.try_into().expect("Fit in u32"),
+                },
+                c,
+            );
         }
     }
     map
 }
 
-fn solve_garden(map: HashMap<Point, char>) -> Garden {
-    let mut perimeters = HashMap::new();
-    let mut regions = vec![];
-    let mut points: HashSet<_> = map.keys().clone().map(|&point| point).collect();
+struct GardenSolver {
+    map: HashMap<Point, char>,
+    perimeters: HashMap<Point, u32>,
+    regions: Vec<GardenRegion>,
+    points: HashSet<Point>,
+}
 
-    while !points.is_empty() {
-        let region_point = *points.iter().next().expect("Not empty");
-        let region_plant = *map.get(&region_point).expect("Exists");
+impl GardenSolver {
+    fn new(map: HashMap<Point, char>) -> Self {
+        let points: HashSet<Point> = map.keys().clone().map(|&point| point).collect();
+        Self {
+            map,
+            perimeters: HashMap::new(),
+            regions: vec![],
+            points,
+        }
+    }
+
+    fn is_corner(&mut self, point: Point, dir_pair: &[Point; 2], region_plant: char) -> bool {
+        let neighbours = dir_pair.iter().map(|dir| point + *dir);
+        let neighbour_plants: Vec<_> = neighbours
+            .map(|neighbour| self.map.get(&neighbour))
+            .collect();
+        if !neighbour_plants.contains(&Some(&region_plant)) {
+            return true;
+        }
+        if neighbour_plants
+            .iter()
+            .all(|&plant| plant == Some(&region_plant))
+        {
+            let diagonal_dir = dir_pair[0] + dir_pair[1];
+            let diagonal_neighbour = point + diagonal_dir;
+            let diagonal_neighbour_plant = self.map.get(&diagonal_neighbour);
+            match diagonal_neighbour_plant {
+                Some(&plant) if plant == region_plant => return false,
+                _ => return true,
+            }
+        }
+        false
+    }
+
+    fn is_perimeter(&self, neighbour: Point, region_plant: char) -> bool {
+        let neighbour_plant = self.map.get(&neighbour);
+        match neighbour_plant {
+            Some(&plant) if plant == region_plant => false,
+            _ => true,
+        }
+    }
+
+    fn solve_region(&mut self) {
+        let region_point = *self.points.iter().next().expect("Not empty");
+        let region_plant = *self.map.get(&region_point).expect("Exists");
 
         let mut region_points = HashSet::new();
         region_points.insert(region_point);
         let mut region_points_to_check = vec![region_point];
+
         let mut corners = 0;
 
-        loop {
-            if region_points_to_check.is_empty() {
-                break;
-            }
+        while !region_points_to_check.is_empty() {
             let next_point = region_points_to_check.pop().expect("Not empty");
-            points.take(&next_point);
-
+            self.points.take(&next_point);
 
             let mut perimeter = 0;
-            for dirs in DIRECTIONS.iter() {
-                {
-                    let neighbours = dirs.iter().map(|dir| next_point + *dir);
-                    let neighbour_plants: Vec<_> = neighbours.map(|neighbour| map.get(&neighbour)).collect();
-                    if !neighbour_plants.contains(&Some(&region_plant)) {
-                        corners += 1;
-                    }
-                    else if neighbour_plants.iter().all(|&plant| plant == Some(&region_plant)) {
-                        let diagonal_dir = dirs[0] + dirs[1];
-                        let diagonal_neighbour = next_point + diagonal_dir;
-                        let diagonal_neighbour_plant = map.get(&diagonal_neighbour);
-                        match diagonal_neighbour_plant {
-                            Some(&plant) if plant == region_plant => (),
-                            _ => {
-                                corners += 1;
-                            }
-                        }
-                    }
+            for dir_pair in DIRECTIONS.iter() {
+                if self.is_corner(next_point, dir_pair, region_plant) {
+                    corners += 1;
                 }
 
-                {
-                    let neighbour = next_point + dirs[0];
-                    let neighbour_plant = map.get(&neighbour);
-                    match neighbour_plant {
-                        Some(&plant) if plant == region_plant => {
-                            if !region_points.contains(&neighbour) {
-                                region_points.insert(neighbour);
-                                region_points_to_check.push(neighbour);
-                            }
-                        }
-                        _ => {
-                            perimeter += 1;
-                        }
-                    }
+                let neighbour = next_point + dir_pair[0];
+
+                if self.is_perimeter(neighbour, region_plant) {
+                    perimeter += 1;
+                } else if !region_points.contains(&neighbour) {
+                    region_points.insert(neighbour);
+                    region_points_to_check.push(neighbour);
                 }
             }
 
-            perimeters.insert(next_point, perimeter);
+            self.perimeters.insert(next_point, perimeter);
         }
 
-        regions.push(GardenRegion {
+        self.regions.push(GardenRegion {
             region: region_points,
             corners,
         });
     }
 
+    fn solve(&mut self) {
+        while !self.points.is_empty() {
+            self.solve_region();
+        }
+    }
+}
+
+fn solve_garden(map: HashMap<Point, char>) -> Garden {
+    let mut solver = GardenSolver::new(map);
+    solver.solve();
     Garden {
-        regions,
-        perimeters,
+        regions: solver.regions,
+        perimeters: solver.perimeters,
     }
 }
 
@@ -132,7 +169,11 @@ fn calculate_price(garden: Garden) -> GardenPrice {
     let mut corner_price = 0;
 
     for garden_region in garden.regions.iter() {
-        let area: u32 = garden_region.region.len().try_into().expect("Fits into u32");
+        let area: u32 = garden_region
+            .region
+            .len()
+            .try_into()
+            .expect("Fits into u32");
 
         let perimeter = garden_region
             .region
@@ -142,7 +183,6 @@ fn calculate_price(garden: Garden) -> GardenPrice {
 
         perimeter_price += area * perimeter;
         corner_price += area * garden_region.corners;
-
     }
 
     GardenPrice {
@@ -156,7 +196,6 @@ fn solve(input: &str) -> GardenPrice {
     let garden = solve_garden(map);
     calculate_price(garden)
 }
-
 
 pub fn part_one(input: &str) -> Option<u32> {
     Some(solve(input).perimeter)
