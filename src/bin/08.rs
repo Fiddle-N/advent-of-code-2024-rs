0bf02650c29c32e276e16564a4138954deba792c
Fadil Nohur <fadil.nohur@outlook.com>
2024-12-14 00:09:41 +0000

    Advent of Code Year 2024 Day 8
    - Refactor

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -34,80 +34,108 @@ impl Sub for Point {
     }
 }
 
-fn solve(input: &str, resonant_harmonics: bool) -> u32 {
-    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-    let height = grid.len().try_into().expect("Height fits into i32");
-    let width = grid[0].len().try_into().expect("Width fits into i32");
+struct CityScanner {
+    grid: Vec<Vec<char>>,
+    height: i32,
+    width: i32,
+    resonant_harmonics: bool,
+}
 
-    let boundary_check =
-        |point: Point| (point.x >= 0 && point.x < width) && (point.y >= 0 && point.y < height);
+impl CityScanner {
+    fn new(input: &str, resonant_harmonics: bool) -> Self {
+        let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+        let height = grid.len().try_into().expect("Height fits into i32");
+        let width = grid[0].len().try_into().expect("Width fits into i32");
 
-    let mut antennas = HashMap::new();
-    for (y, row) in grid.iter().enumerate() {
-        for (x, cell) in row.iter().enumerate() {
-            if *cell == '.' {
-                continue;
-            }
-            let point = Point {
-                x: x.try_into().expect("x fits into i32"),
-                y: y.try_into().expect("y fits into i32"),
-            };
-            let antenna_points = antennas.entry(*cell).or_insert(vec![]);
-            antenna_points.push(point);
+        Self {
+            grid,
+            height,
+            width,
+            resonant_harmonics,
         }
     }
 
-    let mut antinodes = HashSet::new();
-    for points in antennas.into_values() {
-        for (point_1, point_2) in points
-            .into_iter()
-            .combinations(2)
-            .map(|pair| (pair[0], pair[1]))
-        {
-            if resonant_harmonics {
-                antinodes.insert(point_1);
-                antinodes.insert(point_2);
-            }
-            let delta = point_1 - point_2;
-
-            let mut antinode_1 = point_1;
-            loop {
-                antinode_1 = antinode_1 + delta;
-                if !boundary_check(antinode_1) {
-                    break;
-                }
-                antinodes.insert(antinode_1);
-                if !resonant_harmonics {
-                    break;
-                };
-            }
-
-            let mut antinode_2 = point_2;
-            loop {
-                antinode_2 = antinode_2 - delta;
-                if !boundary_check(antinode_2) {
-                    break;
-                }
-                antinodes.insert(antinode_2);
-                if !resonant_harmonics {
-                    break;
-                };
-            }
-        }
+    fn boundary_check(&self, point: Point) -> bool {
+        (point.x >= 0 && point.x < self.width) && (point.y >= 0 && point.y < self.height)
     }
 
-    antinodes
-        .len()
-        .try_into()
-        .expect("Antinodes count fits into u32")
+    fn find_antinodes(&self, antennas: HashMap<char, Vec<Point>>) -> HashSet<Point> {
+        let mut antinodes = HashSet::new();
+        for points in antennas.into_values() {
+            for (point_1, point_2) in points
+                .into_iter()
+                .combinations(2)
+                .map(|pair| (pair[0], pair[1]))
+            {
+                if self.resonant_harmonics {
+                    // antinodes occur at the points of antenna pairs
+                    antinodes.insert(point_1);
+                    antinodes.insert(point_2);
+                }
+                let delta = point_1 - point_2;
+
+                let mut antinode_1 = point_1;
+                loop {
+                    antinode_1 = antinode_1 + delta;
+                    if !self.boundary_check(antinode_1) {
+                        break;
+                    }
+                    antinodes.insert(antinode_1);
+                    if !self.resonant_harmonics {
+                        break;
+                    };
+                }
+
+                let mut antinode_2 = point_2;
+                loop {
+                    antinode_2 = antinode_2 - delta;
+                    if !self.boundary_check(antinode_2) {
+                        break;
+                    }
+                    antinodes.insert(antinode_2);
+                    if !self.resonant_harmonics {
+                        break;
+                    };
+                }
+            }
+        }
+        antinodes
+    }
+
+    fn find_antennas(&self) -> HashMap<char, Vec<Point>> {
+        let mut antennas = HashMap::new();
+        for (y, row) in self.grid.iter().enumerate() {
+            for (x, cell) in row.iter().enumerate() {
+                if *cell == '.' {
+                    continue;
+                }
+                let point = Point {
+                    x: x.try_into().expect("x fits into i32"),
+                    y: y.try_into().expect("y fits into i32"),
+                };
+                let antenna_points = antennas.entry(*cell).or_insert(vec![]);
+                antenna_points.push(point);
+            }
+        }
+        antennas
+    }
+
+    fn solve(&self) -> u32 {
+        let antennas = self.find_antennas();
+        let antinodes = self.find_antinodes(antennas);
+        antinodes
+            .len()
+            .try_into()
+            .expect("Antinodes count fits into u32")
+    }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(solve(input, false))
+    Some(CityScanner::new(input, false).solve())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    Some(solve(input, true))
+    Some(CityScanner::new(input, true).solve())
 }
 
 #[cfg(test)]
